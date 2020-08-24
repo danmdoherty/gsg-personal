34d625668e96a314951a12654865d31b6bd05153
Daniel Doherty <danmdoherty@users.noreply.github.com>
2020-08-24 10:10:55 -0700

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `http://danieldoherty.xyz`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://danieldoherty.xyz`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
